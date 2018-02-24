2354a8d197ac9d67e60c9cf77fcaed8102b184bb
vinaysanthosham <35549856+vinaysanthosham@users.noreply.github.com>
2018-02-23 21:05:24 -0600

    login page

---
+++ ASE Project/Source/src/pages/login/login.ts
@@ -0,0 +1,42 @@
+import { Component,ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams,AlertController } from 'ionic-angular';
+import {MainPage} from '../main/main';
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+
+    @ViewChild('username') user;
+    @ViewChild('password') password;
+    
+  constructor(public navCtrl: NavController, public navParams: NavParams,public alertCtrl: AlertController) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+  signInUser(){
+    
+    if(this.user.value == "mavericks" && this.password.value == "mavericks"){
+                 this.navCtrl.push(MainPage);    
+    }else{
+         let alert = this.alertCtrl.create({
+          title: 'Login unsuccessful!',
+          subTitle: 'Username or Password incorrect!!',
+          buttons: ['OK']
+        });
+        alert.present();
+    }
+    console.log(this.user.value,this.password.value);
+  }
+
+}
