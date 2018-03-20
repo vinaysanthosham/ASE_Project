3ef5a3e70719751a76549b969a48908deefec3db
sunnyvineethreddy <sunnyvineethreddy@gmail.com>
2018-03-19 22:36:28 -0500

    inc2

--- ASE Project/Source/src/pages/home/home.ts
+++ ASE Project/Source/src/pages/home/home.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController,AlertController } from 'ionic-angular';
-import {LoginPage} from '../login/login';
-import {RegisterPage} from '../register/register';
-
+import { HttpClient } from '@angular/common/http';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import { AlertController,LoadingController,ToastController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -10,13 +9,97 @@ import {RegisterPage} from '../register/register';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController,public alertCtrl:AlertController) {
-  
+  public ROOT_URL = 'https://vision.googleapis.com';
+  public API_KEY = 'AIzaSyDCeSjtkgELutZ4prPZBCjKPvw7RIQF3V8'; // YOUR CLOUD PLATFORM API KEY
+  public visionRequest = {
+    "requests": [{
+        "image": {
+          "content": ""
+        },
+        "features": [{
+            "type": "TEXT_DETECTION",
+            "maxResults": 10
+        }]
+    }]
+  };
+  public visionResult = {
+    detectionConfidence: null
+  };
+  public imageResult = 'https://i.imgur.com/hyRulHk.gif';
+
+  constructor(
+    public http: HttpClient,
+    public camera: Camera,
+    public loadingCtrl: LoadingController,
+    public alertCtrl: AlertController,
+    public toastCtrl: ToastController
+  ) {}
+
+  capture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+
+    let loader = this.loadingCtrl.create({
+      content: "Processing..."
+    });
+
+    loader.present(); // show loading component
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.visionRequest.requests[0].image.content = imageData;
+      this.imageResult = 'data:image/jpeg;base64,' + imageData;
+      this.http.post(`${this.ROOT_URL}/v1/images:annotate?key=${this.API_KEY}`, this.visionRequest)
+      .subscribe((data: any) => {
+        loader.dismiss(); // hide loading component
+        //var result = data.responses[0];
+        var length = data.responses[0].textAnnotations.length;
+        var result = data.responses[0].textAnnotations[0].description;
+        this.visionResult.detectionConfidence = result;
+       /*let alert = this.alertCtrl.create({
+          title: 'Ionic Vision',
+          subTitle: data.responses[0].textAnnotations,
+          buttons: ['OK']
+        });
+        alert.present();*/
+      }, (err) => {
+        loader.dismiss(); // hide loading component
+        this.reset();
+        this.showAlert('Vision API Request Error.');
+        console.log("error occured");
+      })
+    }, (err) => {
+      loader.dismiss(); // hide loading component
+      this.reset();
+      this.showAlert('Camera Error Occured.');
+      console.log(err);
+    });
   }
-    signIn(){
-        this.navCtrl.push(LoginPage);    
-    }
-    register(){
-        this.navCtrl.push(RegisterPage);    
-    }
+
+  showAlert(message) {
+    let alert = this.alertCtrl.create({
+      title: 'Ionic Vision',
+      subTitle: message,
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+  presentToast(message) {
+    let toast = this.toastCtrl.create({
+      message,
+      duration: 3000
+    });
+    toast.present();
+  }
+
+  reset() {
+    this.visionResult = {
+      detectionConfidence: null
+    };
+    this.imageResult = 'https://i.imgur.com/hyRulHk.gif';
+  }
 }
